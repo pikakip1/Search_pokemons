fbb006bb52933023502c92d44bbd8ec1c7b47902
pikakip1 <pikakip222@mail.ru>
2023-08-16 18:18:25 +0300

    Add specs to PokemonEntity model

--- pokemon_map-master/pokemon_entities/models.py
+++ pokemon_map-master/pokemon_entities/models.py
@@ -11,7 +11,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
     lat = models.FloatField()
     lon = models.FloatField()
+
     appeared_at = models.DateTimeField()
     disappeared_at = models.DateTimeField()
+
+    level = models.IntegerField(default=1)
+    health = models.IntegerField(default=100)
+    strength = models.IntegerField(default=100)
+    defence = models.IntegerField(default=100)
+    stamina = models.IntegerField(default=100)
