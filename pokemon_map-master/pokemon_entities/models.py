18cd0614074e3974528cdb6c7b06c8f822bbb265
pikakip1 <pikakip222@mail.ru>
2023-08-16 16:32:15 +0300

    Add Pokemon.image model field

--- pokemon_map-master/pokemon_entities/models.py
+++ pokemon_map-master/pokemon_entities/models.py
@@ -3,4 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
+    photo = models.ImageField(upload_to='pokemons_photo/bulbazavr.png', blank=True)
+
+    def __str__(self):
+        return self.title
 
