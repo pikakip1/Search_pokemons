8d470854ff385cc64297ec3a2bb22f5e9ab3397a
pikakip1 <pikakip222@mail.ru>
2023-08-20 17:39:39 +0300

    Added required parameters and removed the description from the model.

--- pokemon_map-master/pokemon_entities/models.py
+++ pokemon_map-master/pokemon_entities/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models  # noqa F401
 
 from django.utils import timezone
@@ -13,55 +15,29 @@ class Pokemon(models.Model):
         verbose_name='Фото покемона',
         blank=True)
 
-    translate_name = {
-        'Бульбазавр':
-            {
-                "title_en": "Bulbasaur",
-                "title_jp": "フシギダネ"
-            },
+    eng_name = models.CharField(
+        max_length=30,
+        verbose_name='Перевод на английский',
+        blank=True
+    )
 
-        'Ивизавр':
-            {
-                "title_en": "Ivysaur",
-                "title_jp": "フシギソウ",
-            },
+    jap_name = models.CharField(
+        max_length=30,
+        verbose_name='Перевод на японский',
+        blank=True
+    )
 
-        'Венузавр':
-            {
-                "title_en": "Venusaur",
-                "title_jp": "フシギバナ",
-            }
-    }
-
-    BULVASAUR_DESCRIPTION = '''
-            cтартовый покемон двойного травяного и ядовитого типа из первого поколения и региона\
-            Канто. В национальном покедексе под номером 1. На 16 уровне эволюционирует в Ивизавра. Ивизавр на 32\
-            уровне эволюционирует в Венузавра. Наряду с Чармандером и Сквиртлом, Бульбазавр является\
-            одним из трёх стартовых покемонов региона Канто.\
-        '''
-
-    IVIZAUR_DESCRIPTION = '''
-            покемон двойного травяного и ядовитого типа из первого поколения покемонов. Эволюционирует из стартового\
-            покемона Бульбазавра на 16 уровне. Становится Венузавром на 32 уровне.
-    '''
-
-    VENUZAUR_DESCRIPTION = '''
-            покемон двойного травяного и ядовитого типа из первого поколения покемонов. На 32 уровне эволюционирует из\
-            Ивизавра. Финальная эволюция травяного стартовика Бульбазавра. Развивается в Мега Венузавра с помощью камня\
-            Венусарита.
-    '''
-
-    DESCRIPTION = {
-        'Бульбазавр': BULVASAUR_DESCRIPTION,
-        'Ивизавр': IVIZAUR_DESCRIPTION,
-        'Венузавр': VENUZAUR_DESCRIPTION
-    }
+    description = models.CharField(
+        max_length=1000,
+        verbose_name='Описание',
+        blank=True
+    )
 
     evolution_from = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
         verbose_name='Эволюционировал из',
-        related_name='evolution',
+        related_name='next_evolution',
         blank=True,
         null=True
     )
@@ -74,6 +50,7 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
+        related_name='pokemon_entity',
         verbose_name='Название покемона',
         )
 
@@ -81,13 +58,15 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Долгота')
 
     appeared_at = models.DateTimeField(verbose_name='Дата появления')
-    disappeared_at = models.DateTimeField(verbose_name='Дата исчезновения', blank=True,null=True, default=timezone.now)
+    disappeared_at = models.DateTimeField(
+        verbose_name='Дата исчезновения',
+    )
 
-    level = models.IntegerField(default=1,verbose_name='Уровень', null=True,blank=True)
-    health = models.IntegerField(default=100, verbose_name='Здоровье',null=True,blank=True)
-    strength = models.IntegerField(default=100, verbose_name='Сила', null=True,blank=True)
-    defence = models.IntegerField(default=100, verbose_name='Защита', null=True,blank=True)
-    stamina = models.IntegerField(default=100, verbose_name='Выносливость', null=True, blank=True)
+    level = models.IntegerField(verbose_name='Уровень', null=True,blank=True)
+    health = models.IntegerField(verbose_name='Здоровье',null=True,blank=True)
+    strength = models.IntegerField(verbose_name='Сила', null=True,blank=True)
+    defence = models.IntegerField(verbose_name='Защита', null=True,blank=True)
+    stamina = models.IntegerField(verbose_name='Выносливость', null=True, blank=True)
 
     def __str__(self):
         return self.pokemon.title
